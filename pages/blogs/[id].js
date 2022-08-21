b9b9585c5579f2789b8f0f0065961db6be4e5646
Ahmed Mohammed <ahmed.mohd200130@gmail.com>
2022-08-21 08:12:37 +0200

    add 404 page

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -9,7 +9,7 @@ export default function Blog() {
   const { id } = router.query
   return (
     <Fragment>
-      <li key={Date.now()} className="flex flex-col align-center w-full 
+      {id<blogs.length?<li key={Date.now()} className="flex flex-col align-center w-full 
       rounded my-5 px-2 text-slate-900 cursor-pointer
       lg:w-full lg:flex lg:flex-col lg:px-48
       md:flex md:flex-col md:px-20
@@ -21,7 +21,12 @@ export default function Blog() {
       <span> {blogs[id].date}</span>
       <Image className='rounded ' src={blogs[id].img} alt={blogs[id].title} width={700} height={300} />
         <p className=' my-1 '>{blogs[id].paragraphs}</p>
-      </li> 
+      </li> :
+      <div className='absolute top-0 w-full h-full flex items-center justify-center bg-white '>
+      <h2 className='text-slate-900 text-2xl flex items-center justify-center'><strong className='text-slate-900 text-5xl border-r-2 px-1 mx-5'>404</strong>This page could not be found</h2>  
+      </div>
+      }
+      
       </Fragment>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './pages/index.js',
     './pages/admin/index.js',
+    './pages/blogs/[id].js',
     './components/Navbar.js',
   ],
   theme: {
